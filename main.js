a4b23d7b5a5890f58dd3a3939b3727da74d81520
Jhon <Deityss@gmail.com>
2024-09-01 13:22:28 -0500

    change_color

---
+++ main.js
@@ -0,0 +1,7 @@
+const fecha=document.querySelector('#fecha');
+const boton = document.querySelector('#cambiar-color')
+
+boton.addEventListener('click', () => {
+    fecha.style.background='lightblue'
+
+})
